fb7a7c4a2ad3b9a2e6e6b57197fa44ba1d26b9be
Hiromi Kawanishi <contacthiromi@gmail.com>
2016-02-25 01:07:25 +0000

    can create a message

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Grammable::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root "grams#index"
+
+  resources :grams, only: [:new, :create]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
